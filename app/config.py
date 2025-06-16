d9cabd25a7df5b7e10c4508f9bf3a383973b7255
s33ding <robertomdiniz@protonmail.com>
2025-06-16 13:57:35 -0300

    fixed

--- app/config.py
+++ app/config.py
@@ -1,6 +1,8 @@
+
+
 city="Cidade Estrutural, Distrito Federal, Brazil"
 api_key="teste"
-gdf_file_name="output/roads.geojson"
+gdf_file_name="/app/output/roads.geojson"
 bucket_name = "myapp-govsananga"
 dynamo_tbl_1 = "govSananga-main"
 dynamo_tbl_2 = "govSananga-img-analysis"
@@ -8,6 +10,6 @@ region = "us-east-1"
 limit_loop = 3
 lambda_1_label_img="arn:aws:lambda:us-east-1:408411726795:function:govSananga-label"
 gsv_fov = 65
-img_repo_path = "media/gpt-img-repo.webp"
-img_map_path = "static/images/realistic_road_network_map.png"
+img_repo_path = "/app/media/gpt-img-repo.webp"
+img_map_path = "/app/static/images/realistic_road_network_map.png"
 region_name = "us-east-1"
